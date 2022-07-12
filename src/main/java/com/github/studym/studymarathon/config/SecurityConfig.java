e432074624f3657eb897d5e51b7a8ee607a3009f
Crow <oneso123456789@gmail.com>
2022-07-12 19:09:34 +0900

    api 로그인 apiLoginFilter문제 해결 & JWT 토큰 생성 테스트 추가

--- src/main/java/com/github/studym/studymarathon/config/SecurityConfig.java
+++ src/main/java/com/github/studym/studymarathon/config/SecurityConfig.java
@@ -1,14 +1,16 @@
 package com.github.studym.studymarathon.config;
 
-import com.github.studym.studymarathon.config.handler.MemberLoginSuccessHandler;
 import com.github.studym.studymarathon.security.filter.ApiCheckFilter;
 import com.github.studym.studymarathon.security.filter.ApiLoginFilter;
+import com.github.studym.studymarathon.security.handler.ApiLoginFailHandler;
+import com.github.studym.studymarathon.security.handler.MemberLoginSuccessHandler;
 import com.github.studym.studymarathon.security.service.AuthUserDetailsService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,11 +29,15 @@ public class SecurityConfig {
     @Autowired
     private AuthUserDetailsService userDetailsService;
 
-
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+
+
+
 
 /*    @Bean // 내부적으로 테스트용으로 만든 계정 비밀번호는 1234 DB를 설계한후 Test코드로 더미 유저를 넣으면 사용하지 않음
     public InMemoryUserDetailsManager user() {
@@ -55,8 +61,10 @@ public class SecurityConfig {
         http.oauth2Login().successHandler(successHandler());
         http.rememberMe().tokenValiditySeconds(60 * 10).userDetailsService(userDetailsService);//10분짜리 로그인 유지 토큰
 
+        AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+
         http.addFilterBefore(apiCheckFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.addFilterBefore(apiLoginFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(apiLoginFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
@@ -68,18 +76,29 @@ public class SecurityConfig {
 
 
     @Bean
-    public ApiLoginFilter apiLoginFilter() throws Exception {
+    public ApiLoginFilter apiLoginFilter(AuthenticationManager authenticationManager) throws Exception {
 
         ApiLoginFilter apiLoginFilter = new ApiLoginFilter("/api/login");
-        apiLoginFilter.setAuthenticationManager(authentication -> authentication);
+        apiLoginFilter.setAuthenticationManager(authenticationManager);
 
-
+        apiLoginFilter.setAuthenticationFailureHandler(new ApiLoginFailHandler());
 
         return apiLoginFilter;
     }
 
     @Bean
-    public ApiCheckFilter apiCheckFilter() {
-        return new ApiCheckFilter("/api/member/*");
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
+
+
+    @Bean
+    public ApiCheckFilter apiCheckFilter() {
+        return new ApiCheckFilter("/sample/**/*");
+    }
+
+
 }
+
+
--- src/main/java/com/github/studym/studymarathon/security/util/JWTUtil.java
+++ src/main/java/com/github/studym/studymarathon/security/util/JWTUtil.java
@@ -1,13 +1,11 @@
 package com.github.studym.studymarathon.security.util;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.crypto.spec.SecretKeySpec;
@@ -19,27 +17,33 @@ import java.util.Date;
 
 @Log4j2
 public class JWTUtil {
+
     @Autowired
     private UserDetailsService userDetailsService;
 
-    private String secret = "studymarathon";
-    byte[] secretBytes = DatatypeConverter.parseBase64Binary(secret);
-    Key secretKey = new SecretKeySpec(secretBytes, SignatureAlgorithm.HS256.getJcaName());
+    // private  String secretKey = "studymarathon";
+
+    // specified key byte array is 256 bits 이상으로 지정해야함
+    private Key key = Keys.hmacShaKeyFor("studymarathon12345678studymarathon12345678".getBytes(StandardCharsets.UTF_8));
+    /*Key secretKey = new SecretKeySpec(secretBytes, SignatureAlgorithm.HS256.getJcaName());*/
     //유효기간
     private long expire = 60 * 10;
 
-    public String generateToken(Authentication authentication) throws Exception{
+    public String generateToken(String authentication) throws Exception{
 
-        Claims claims = Jwts.claims().setSubject(authentication.getName());
 
-        Date now = new Date();
-        Date expiresIn = new Date(now.getTime() + expire);
-
-        return Jwts.builder()
+/*        return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(expiresIn)
                 .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();*/
+
+        return Jwts.builder()
+                .setIssuedAt(new Date())
+                .setExpiration(Date.from(ZonedDateTime.now().plusMinutes(expire).toInstant()))
+                .setSubject("Test")
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
@@ -48,7 +52,7 @@ public class JWTUtil {
 
         try{
 
-            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(tokenStr);
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(tokenStr);
             return true;
         } catch (JwtException e) {
             e.printStackTrace();
