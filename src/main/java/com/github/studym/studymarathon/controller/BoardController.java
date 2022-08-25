0910fcaf49403ee4fba2d455c05335040f92e45d
Crow <oneso123456789@gmail.com>
2022-08-25 19:18:53 +0900

    415 에러

--- src/main/java/com/github/studym/studymarathon/controller/BoardController.java
+++ src/main/java/com/github/studym/studymarathon/controller/BoardController.java
@@ -1,15 +1,24 @@
 package com.github.studym.studymarathon.controller;
 
 import com.github.studym.studymarathon.domain.board.dto.BoardDTO;
+import com.github.studym.studymarathon.domain.board.dto.MultipartFileDTO;
 import com.github.studym.studymarathon.domain.board.dto.PageRequestDTO;
 import com.github.studym.studymarathon.domain.board.dto.PageResultDTO;
 import com.github.studym.studymarathon.domain.board.entity.Board;
 import com.github.studym.studymarathon.domain.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import net.coobird.thumbnailator.Thumbnailator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
@@ -18,6 +27,9 @@ import org.springframework.web.bind.annotation.*;
 public class BoardController {
 
     private final BoardService service;
+
+    @Value("${com.github.studym.studymarathon.path}")
+    private String uploadPath;
 
     @GetMapping("/list")
     public PageResultDTO<BoardDTO, Board> list() {
@@ -30,12 +42,35 @@ public class BoardController {
 
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<Long> register(@RequestBody BoardDTO dto) {
+    @PostMapping(value = "/register", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public void register(BoardDTO dto, @RequestPart(required = false) MultipartFileDTO fileDTO) {
+        service.register(dto);
 
-        Long bno = service.register(dto);
+        log.info("/BoardController/register");
+        log.info("boardDTO: " + dto);
+        log.info("fileDTO: " + fileDTO);
 
-        return new ResponseEntity<>(bno, HttpStatus.OK);
+        if (fileDTO != null) {
+            fileDTO.getFiles().forEach(multipartFile -> {
+                String originalFilename = multipartFile.getOriginalFilename();
+                log.info(originalFilename);
+                String uuid = UUID.randomUUID().toString();
+                Path savePath = Paths.get(uploadPath, uuid + "_" + originalFilename);
+
+
+                try {
+                    multipartFile.transferTo(savePath);
+
+                    if (Files.probeContentType(savePath).startsWith("image")) {
+                        File thumbFile = new File(uploadPath, "s_" + uuid + "_" + originalFilename);
+
+                        Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200, 200);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
     }
 
 }
